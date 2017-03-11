3ae2f8c25ede84b7664ca15581a8fac01401c970
TemaMix <tema.place@gmail.com>
2017-03-11 22:15:28 +0300

    Generate Groups Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'groups/index'
+
+  get 'groups/create'
+
+  get 'groups/destroy'
+
+  get 'groups/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
